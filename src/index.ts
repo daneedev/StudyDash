00173498541cd0f806c04b9869a8f4abcef59514
Danee <imdanee@seznam.cz>
2025-01-22 09:19:21 +0100

    backend for autorendering the frontend

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import { createServer } from 'http';
+import nunjucks from 'nunjucks';
+import path from 'path';
 dotenv.config();
 
 const app = express();
 const server = createServer(app);
 
+nunjucks.configure('src/views', {
+  autoescape: true,
+  express: app,
+  watch: true
+});
+
+app.use("/", express.static(path.join(__dirname, 'public')));
+
+app.get("/:file", function (req, res) {
+  res.render(req.params.file);
+})
 
 server.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
