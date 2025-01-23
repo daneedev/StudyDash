d1e50408c3c1d8e6764e04ec6a1923d8e2b2ba9d
Richard Kobza <rikobzaa@gmail.com>
2025-01-23 20:31:58 +0100

    html login stranky

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      "yellow": "#FFCD1F",
+      "white": "#F6F7FB",
+      "light-gray": "#586380",
+      "dark-gray": "#2E3856",
+      "dark-blue": "#0A092D",
+
+    },
+
     extend: {},
   },
   plugins: [],
