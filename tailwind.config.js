207abce5bbeedee832a4ba8f37a3097683f9688f
Richard Kobza <rikobzaa@gmail.com>
2025-01-24 22:05:48 +0100

    login in progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    
-
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        background: 'var(--color-background)',
+        text: 'var(--color-text)',
+        darkgray: 'var(--color-darkgray)',
+        lightgray: 'var(--color-lightgray)',
+      },
+      fontFamily: {
+        display: ['var(--font-display)'],
+      },
     extend: {},
   },
   plugins: [],
 }
-
+}
