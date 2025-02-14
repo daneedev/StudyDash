dcb3ddcfd8d0d482c33c3984545a1377cfaba344
daneedev <dan@danee.dev>
2025-02-14 12:58:59 +0100

    passport done

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,13 @@ import { createServer } from 'http';
 import nunjucks from 'nunjucks';
 import path from 'path';
 import Logger from './utils/logger';
+import loadPassport from './utils/passport';
+import session from 'express-session';
+import crypto from 'crypto';
+import fs from 'fs';
+import passport from 'passport';
 dotenv.config();
+console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS);
 
 const app = express();
 const server = createServer(app);
@@ -14,6 +20,29 @@ nunjucks.configure('src/views', {
   express: app,
   watch: true
 });
+
+if (!process.env.SESSION_SECRET) {
+  Logger.logWarning('No SESSION_SECRET found in .env file. Generating one now...');
+  const SESSION_SECRET = crypto.randomBytes(32).toString('hex');
+  fs.appendFileSync('.env', `\nSESSION_SECRET=${SESSION_SECRET}`);
+  process.env.SESSION_SECRET = SESSION_SECRET;
+}
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production'
+  },
+}));
+
+// PASSPORT
+app.use(passport.initialize());
+app.use(passport.session());
+loadPassport();
 
 // DATABASE
 import { connect } from './db';
