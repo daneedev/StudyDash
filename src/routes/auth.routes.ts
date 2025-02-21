169111fdae8fd4776ab959825a3555316ff1fffd
daneedev <dan@danee.dev>
2025-02-21 11:20:36 +0100

    auth routes working

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -3,7 +3,11 @@ import passport from 'passport';
 import User from '../models/User';
 const router = express.Router();
 
-router.get("/login", passport.authenticate('local', {
+router.get("/login", function (req: Request, res: Response) {
+    res.render("login.html");
+})
+
+router.post("/login", passport.authenticate('local', {
     successRedirect: '/dash',
     failureRedirect: '/login',
     failureFlash: true
@@ -13,18 +17,38 @@ router.get("/register", function (req: Request, res: Response) {
     res.render("register.html");
 })
 
-router.post("/register", function (req: Request, res: Response) {
-    const { username, email, password } = req.body;
-    User.create({
-        username: username,
-        email: email,
-        password: password
-    }).then(() => {
+router.post("/register", async (req: Request, res: Response) => {
+    try {
+        const { username, email, password } = req.body;
+        const checkUsername = await User.findOne({ where: { username: username } });
+        const checkEmail = await User.findOne({ where: { email: email } });
+
+        if (checkUsername) {
+            req.flash('error', 'Username already exists');
+            res.redirect('/auth/register');
+            return;
+        } else if (checkEmail) {
+            req.flash('error', 'Email already exists');
+            res.redirect('/auth/register');
+            return;
+        } else if (password.length < 8) {
+            req.flash('error', 'Password must be at least 8 characters');
+            res.redirect('/auth/register');
+            return;
+        }
+
+        const newUser = await User.create({
+            username: username,
+            email: email,
+            password: password
+        });
+
         res.redirect("/auth/login");
-    }).catch((err) => {
-        console.log(err);
-        res.redirect("/auth/register");
-    })
-})
+    } catch (error) {
+        console.error("Error creating user:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
 
 export default router;
