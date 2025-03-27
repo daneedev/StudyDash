c72cbe1b7cee9e8160c2a3fd99fb5cef28dd1f30
daneedev <dan@danee.dev>
2025-03-27 17:50:43 +0100

    DB_PORT added in connection

--- src/db.ts
+++ src/db.ts
@@ -12,6 +12,7 @@ if (!process.env.DB_NAME || !process.env.DB_USER || !process.env.DB_PASS || !pro
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     dialect: 'mariadb',
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
     logging: false
   })
   
