b45bb76f0846c45d54951f99ebc5bfd8e04f6f9e
unknown <kobza.ri.2023@skola.ssps.cz>
2025-04-30 13:34:41 +0200

    added gauge chart using chartjs on classpage indicating percentage of completion

--- src/public/js/class.js
+++ src/public/js/class.js
@@ -18,3 +18,50 @@ document.addEventListener('DOMContentLoaded', () => {
         e.stopPropagation();
     });
 });
+
+
+
+const ctx = document.getElementById('gaugeChart').getContext('2d');
+
+const gaugeChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ['Hotovo', 'Zbývá'], //vrchní labels
+    datasets: [{
+      data: [90, 10], // [procento uspesnosti, zbytek]
+      backgroundColor: ['#3b82f6', '#e5e7eb'], // blue and light gray
+      borderWidth: 0
+    }]
+  },
+  options: {
+    cutout: '70%', //sirka cary (mensi procento -> sirsi)
+    rotation: -90,
+    circumference: 180,
+    plugins: {
+      legend: {
+        display: true
+      },
+      tooltip: {
+        enabled: false
+      }
+    }
+  },
+  plugins: [{
+id: 'centerText',
+beforeDraw(chart, args, options) {
+  const { width, height, ctx } = chart;
+
+  ctx.save();
+  const text = '70%';
+  ctx.font = 'bold 3rem sans-serif';
+  ctx.textBaseline = 'middle';
+  ctx.textAlign = 'center';
+  ctx.fillStyle = '#ffffff';
+  ctx.fillText(text, width / 2, height / 1.2);
+  ctx.restore();
+}
+}]
+
+});
+
+
