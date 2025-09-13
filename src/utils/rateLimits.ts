abd2abc704ca1259ebde5a481660d52feac000dc
daneedev <dan@danee.dev>
2025-09-13 12:47:37 +0200

    react + express tech stack

--- src/utils/rateLimits.ts
+++
@@ -1,9 +0,0 @@
-import rateLimit from 'express-rate-limit';
-
-const globalLimiter = rateLimit({
-    windowMs: 1 * 60 * 1000,
-    max: 100,
-    message: 'Too many requests from this IP, please try again after 15 minutes'
-});
-
-export default { globalLimiter };
