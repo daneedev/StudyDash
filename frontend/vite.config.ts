4c23c338fdc8304413143f5a7b894ffdd5b512fb
Richard Kobza <rikobzaa@gmail.com>
2025-10-13 19:40:03 +0200

    tailwindcss config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   // Load environment variables from parent directory
   const env = loadEnv(mode, "../", "");
   
   return {
-    plugins: [react()],
+    plugins: [react(),tailwindcss(),],
     server: {
       port: Number(env.FRONTEND_PORT) || 3000,
     },
