05289a959a12ba831e8b4510a97c274d1b949502
daneedev <dan@danee.dev>
2025-11-04 23:16:02 +0100

    docker deployment added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   // Load environment variables from parent directory
   const env = loadEnv(mode, "../", "");
-  
+
   return {
-    plugins: [react(),tailwindcss(),],
+    plugins: [react(), tailwindcss()],
     server: {
       port: Number(env.FRONTEND_PORT) || 3000,
+      host: "0.0.0.0",
+    },
+    preview: {
+      port: Number(env.FRONTEND_PORT) || 3000,
+      host: "0.0.0.0",
     },
     // Configure Vite to load .env from parent directory
     envDir: "../",
