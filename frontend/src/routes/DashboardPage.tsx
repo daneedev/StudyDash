7881818a4be0d7ed7726f300a3b7a877a95b759f
vojta1100 <vojtech1jergl@gmail.com>
2025-12-01 19:44:18 +0100

    Nav bar a příprava na podstránky na dashboardu

--- frontend/src/routes/DashboardPage.tsx
+++ frontend/src/routes/DashboardPage.tsx
@@ -1,17 +1,27 @@
 import { createRoute, redirect, type AnyRoute } from '@tanstack/react-router'
+import "../assets/css/index.css";
+import { useState } from 'react'
 
 import { rootRoute } from './rootRoute'
+import { DashboardNavBar, type DashboardTab } from "../components/DashboardNavBar";
 
-const DashboardPage = () => (
-  <main className="flex min-h-screen items-center justify-center p-6">
-    <div className="space-y-2 text-center">
-      <h1 className="text-2xl font-semibold">Dashboard</h1>
-      <p className="text-gray-500">
-        Protected dashboard placeholder. Users must be authenticated to view this route.
-      </p>
-    </div>
-  </main>
-)
+const DashboardPage = () => {
+  const [activeTab, setActiveTab] = useState<DashboardTab>("dashboard");
+
+  return (
+    <>
+      <DashboardNavBar activeTab={activeTab} onTabChange={setActiveTab} />
+      <main className="flex min-h-screen items-center justify-center p-6">
+        <div className="space-y-2 text-center">
+          <h1 className="text-2xl font-semibold">Dashboard</h1>
+          <p className="text-gray-500">
+            Protected dashboard placeholder. Users must be authenticated to view this route. <br /> {activeTab}
+          </p>
+        </div>
+      </main>
+    </>
+  )
+}
 
 const route = createRoute({
   getParentRoute: () => rootRoute,
